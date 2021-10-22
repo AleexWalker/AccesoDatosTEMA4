9771503074be9eb5e224df63c229ab3c092739a7
alu10194039 <aluurs4039@ieselcaminas.org>
2021-10-22 20:53:51 +0200

    Ejercicio2

--- src/exercicis/Ex4_2_PassarRutesObjSQLite.kt
+++ src/exercicis/Ex4_2_PassarRutesObjSQLite.kt
@@ -16,17 +16,29 @@ fun main(args: Array<String>) {
     val statement = connexio.createStatement()
 
     try {
-        val ruta = ficheroEntrada.readObject() as Ruta
-        arrayRutas.add(ruta)
+        while (true) {
+            val ruta = ficheroEntrada.readObject() as Ruta
+            arrayRutas.add(ruta)
+        }
     } catch (eof : EOFException){
         ficheroEntrada.close()
     }
+    println(arrayRutas[1].nom)
 
-    for (i in arrayRutas.indices)
-        val entradaRutasSQL = " INSERT INTO RUTES VALUES " + " nu"
+    for (i in 0 until arrayRutas.size) {
+        val entradaRutasSQL = "INSERT into RUTES VALUES" + "(${i+1}," + "'${arrayRutas[i].nom}'," + "${arrayRutas[i].desnivell}," + "${arrayRutas[i].desnivellAcumulat})"
+        if (i == 1)
+            statement.executeUpdate(entradaRutasSQL)
+        println(entradaRutasSQL)
+        for (j in 0 until arrayRutas[i].llistaDePunts.size) {
+            val entradaPuntosSQL = "INSERT into PUNTS VALUES" + "(${i+1}," + "${j+1}," + "'${arrayRutas[i].llistaDePunts[j].nom}'," + "${arrayRutas[i].llistaDePunts[j].coord.latitud}," + "${arrayRutas[i].llistaDePunts[j].coord.longitud})"
+            if (i == 1)
+                statement.executeUpdate(entradaPuntosSQL)
+            println(entradaPuntosSQL)
+        }
+    }
+    //println(entradaRutasSQL)
 
-
-    statement.executeUpdate(entradaRutasSQL)
     statement.close()
     connexio.close()
 }
